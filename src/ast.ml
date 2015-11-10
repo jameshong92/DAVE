57eafd850bf6015c481a202a35fcffdb6b691e29
James Hong <hh2473@columbia.edu>
2015-11-10 03:08:49 -0500

    Updated ast, parser and scanner

--- src/ast.ml
+++ src/ast.ml
@@ -12,33 +12,40 @@ type datatype = Int | Float | String | Bool | Rec | Tbl | Fld | Void
 (*Arguments*)
 type arg = datatype * string
 (*Types of References*)
-type ref = ARef | RRef | FRef | TRef
+type ref = ARef | RecRef | FldRef | TblRef
 
 
 (*Expression*)
 (*Critical: Refer to the Issue List Before Proceed*)
-type expr = 
-	Id of string
-	| IntLit of int
+type id = Id of string
+
+type lvalue =
+	Var of id
+	| ArrayElem of id * expr list
+and expr = 
+	IntLit of int
 	| FloatLit of float
 	| StringLit of string
 	| BoolLit of bool
-	| Var of datatype * expr
 	| Binop of expr * binop * expr
+	| AssignOp of lvalue * binop * expr
 	| PrefixUnop of preunop * expr
-	| SuffixUnop of expr * sufunop
-	| Array of string * datatype * expr list
-	| Ref of string * ref * expr list
-	| Assign of string * expr
-	| FuncCall of string * expr list
+	| SuffixUnop of lvalue * sufunop
+	| ArrayRange of expr * expr
+	| Ref of id * ref * expr list
+	| Assign of lvalue * expr
+	| FuncCall of id * expr list
 	(*Tbl = a list of Rec | a list of Fld*)
 	| Tbl of expr list
 	(*Rec = Name of the Rec * (Datatype of Each Component * Name of Each Component) * Value of Each Component*)
 	(*Supplemental: Store Values of Each Component in the Format of String When Coding the Parser*)
-	| Rec of string * reccompoent list * expr list
+	| Rec of datatype * expr list
 	(*Fld = Name of the Field * Datatype of the Field * Values of the List*)
-	| Fld of string * datatype * expr list
+	| Fld of id * datatype * expr list
+	| Lval of lvalue
 	| Noexpr
+
+and var_decl = VarDecl of datatype * expr
 
 (*Statement*)
 type stmt = 
@@ -48,13 +55,14 @@ type stmt =
 	| If of expr * stmt * stmt
 	| For of expr * expr * expr * stmt
 	| While of expr * stmt
+	| Localvar of var_decl
 
 (*Function Declaration*)
 type func_decl = {
 	fname : string;
-	return_type : datatype;
 	formals : arg list;
 	body: stmt list;
+	return_type : datatype;
 }
 
 (*Programs*)
@@ -85,9 +93,6 @@ let string_of_asnop = function
 | Muleq -> "*="
 | Diveq -> "/="
 | Modeq -> "%="
-
-
-
 
 
 
--- src/parser.mly
+++ src/parser.mly
@@ -3,7 +3,7 @@
 %token LPAREN RPAREN LBRACE RBRACE LBRACK RBRACK 
 %token SEMICOL COMMA DOT COLON
 %token ADDEQ SUBEQ MULEQ DIVEQ MODEQ
-%token PLUS MINUS TIMES DIVIDE MOD INC DEC
+%token PLUS MINUS TIMES DIVIDE MOD INC DEC EXP
 %token ASN AND OR NOT
 %token EQ NEQ LT LEQ GT GEQ 
 %token REC FLD TBL
@@ -26,10 +26,52 @@
 %left EQ NEQ
 %left LT LEQ GT GEQ
 %left PLUS MINUS
-%left TIMES DIVIDE MOD
+%left TIMES DIVIDE MOD EXP
 %right NOT
 
 %start program
 %type <Ast.program> program
 
 %%
+
+/* Expressions */
+/* id */
+id:
+	ID { Id($1) }
+
+/* id[list_expr], id[expr:expr] */
+lvalue:
+	id { Var($1) }
+	| id LBRACK list_expr RBRACK { Array($1, $3) }
+	| id LBRACK expr COLON expr RBRACK { ArrayRange($1, $3, $5) }
+
+list_expr:
+	expr 									 { [$1] }
+	| expr COMMA list_expr { $1 :: $3 }
+
+assign_expr:
+	id ASN expr SEMICOL 	{ Assign($1, $3) }
+
+expr:
+	| expr PLUS expr 			{ Binop($1, Add, $3) }
+	| expr MINUS expr 		{ Binop($1, Sub, $3) }
+	| expr TIMES expr 		{ Binop($1, Multi, $3) }
+	| expr DIVIDE expr 		{ Binop($1, Div, $3) }
+	| expr MOD expr 			{ Binop($1, Mod, $3) }
+	| expr EXP expr 			{ Binop($1, Exp, $3) }
+	| expr EQ expr 				{ Binop($1, Equal, $3) }
+	| expr NEQ expr 			{ Binop($1, Neq, $3) }
+	| expr LT expr 				{ Binop($1, Lt, $3) }
+	| expr GT expr 				{ Binop($1, Gt, $3) }
+	| expr LEQ expr 			{ Binop($1, Leq, $3) }
+	| expr GEQ expr 			{ Binop($1, Geq, $3) }
+	| expr AND expr 			{ Binop($1, And, $3) }
+	| expr OR expr 				{ Binop($1, Or, $3) }
+
+	| INT_LIT 						{ IntLit($1) }
+	| FLOAT_LIT 					{ FloatLit($1) }
+	| STR_LIT 					{ StringLit($1) }
+	| BOOL_LIT 						{ BoolLit($1) }
+
+program:
+  /* nothing */ { { stmts = []; funcs = [] } }
