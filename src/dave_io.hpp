9d9976b7d2139c80577020ac93ad0fccedd67802
James Hong <hh2473@columbia.edu>
2015-12-22 11:59:30 -0500

    updating tests and io's

--- src/dave_io.hpp
+++ src/dave_io.hpp
@@ -23,9 +23,8 @@ tbl tbl_read(string filename) {
             int j = 0;
             while (buffer[i+j] != ';') {
                 // note here, every line need to be end with " ;"
-                if (buffer[i+j] == ' ') {
+                if (buffer[i+j] == '\t') {
                     string temp = whole.substr(i, j);
-                    cout << temp << endl;
                     name.push_back(temp);
                     i = i+j+1;
                     j = 0;
@@ -43,7 +42,7 @@ tbl tbl_read(string filename) {
         int i = 0;
         int j = 0;
         while (buffer[i+j] != ';') {
-            if (buffer[i+j] == ' ') {
+            if (buffer[i+j] == '\t') {
                 string temp = whole.substr(i, j);
                 cout << temp << endl;
                 element.push_back(temp);
@@ -61,7 +60,7 @@ tbl tbl_read(string filename) {
         storage.push_back(newrec);
     }
     in.close();
-    tbl readtbl = tbl(&storage[0], storage[0].length, storage.size());
+    tbl readtbl = tbl(&storage[0], storage.size(), storage[0].length);
     return readtbl;
 }
 
@@ -69,41 +68,40 @@ void tbl_write(tbl &in, string filename) {
     ofstream out;
     out.open(filename);
     for (int i=0; i<in.row_length; i++) {
-        if (i<in.row_length-1) {
-            out << in.t[i].name << "\t";
-        } else {
-            out << in.t[i].name << endl;
-        }
+        out << in.t[i].name << "\t";
     }
+    out << ";" << endl;
     for (int i=0; i<in.col_length; i++) {
-        out << "[";
         for (int j=0; j<in.row_length; j++) {
             if ( in.t[j].type == 0 ) {
                 out << in.t[j].f_int[i];
-                if (j < in.row_length-1) {
-                    out << ",\t";
-                }
             } else if ( in.t[j].type == 1 ) {
                 out << in.t[j].f_double[i];
-                if (j < in.row_length-1) {
-                    out << ",\t";
-                }
             } else if ( in.t[j].type == 2 ) {
                 out << in.t[j].f_string[i];
-                if (j < in.row_length-1) {
-                    out << ",\t";
+            } else if ( in.t[j].type == 3 ) {
+                if (in.t[j].f_bool[i] == 0) {
+                    out << "false";
+                } else {
+                    out << "true";
                 }
             }
+            out << "\t";
         }
-        out << "]" << endl;
+        if (i != in.col_length-1) {
+            out << ";" << endl;
+        } else {
+            out << ";";
+        }
     }
     out.close();
 }
 
-/*
-int main(int argc, char const *argv[]) {
+/*int main(int argc, char const *argv[]) {
 	tbl test = tbl_read(argv[1]);
 	cout << test;
     tbl_write(test, argv[2]);
+    test = tbl_read(argv[2]);
+    cout << test;
 	return 0;
 }*/
