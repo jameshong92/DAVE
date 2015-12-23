5db6f5082449cc7fe3514c571d4ee86484b1b50f
James Hong <hh2473@columbia.edu>
2015-12-22 22:41:45 -0500

    added bad cases

--- src/test.sh
+++ src/test.sh
@@ -12,10 +12,11 @@ Compare() { #compare the two files
 	    (( success++ ))
 	    echo "PASS"
 	} || {
-	    echo "FAILDED: does not match expected output"
+	    echo "FAILED: does not match expected output"
 	}
 }
 
+TESTING GOOD CASES
 echo "Testing good cases:"
 for f in $TESTFILES
 do
@@ -32,14 +33,50 @@ do
 			Compare "../test/$name.out" "../test/ideal"
 		} || {
 			cat "../test/$name.out"
-			echo "FAILDED: did not compile"
+			echo "FAILED: did not compile"
 		}
 	} || {
 		cat "../test/$name.out"
-		echo "FAILDED: did not compile"
+		echo "FAILED: did not compile"
 	}
 done
 
 echo "SUMMARY"
 echo "Number of tests run: $run"
-echo "Number Passed: $success"
+echo "Number Good Cases Passed: $success"
+echo "######################################"
+
+
+# TESTING BAD CASES
+BADFILES="../test/bad/*.dave"
+badrun=0;
+fail=0;
+
+echo "Testing bad cases:"
+for f in $BADFILES
+do
+	(( badrun++ ))
+	echo "###################"
+	name=${f#$prefix}
+	name=${name%$suffix}
+	ideal="../test/ideal/$name.out" #set the expect output path 
+	rm -f "../test/$name.out" #remove previous output
+	echo "Testing: $name"
+	./dave -c < "$f" 2>&1 && {
+		g++ -w dave.cc -Isrc 2>&1 && {
+			echo "FAILED: bad case successfully compiled"
+		} || {
+			cat "../test/$name.out"
+			echo "PASS"
+			(( fail++ ))
+		}
+	} || {
+		cat "../test/$name.out"
+		echo "PASS"
+		(( fail++ ))
+	}
+done
+
+echo "SUMMARY"
+echo "Number of tests run: $badrun"
+echo "Number of Bad Cases Passed: $fail"
